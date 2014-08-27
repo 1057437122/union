9b425e8e5d1d33e84e7bcb68caa1df5f7d1cf4fd
whitelee <shiqiba7@163.com>
2014-08-27 23:41:48 +0800

    change the index page show way

--- application/views/zone/lists.php
+++ application/views/zone/lists.php
@@ -1,15 +1,5 @@
-<div class="meta">已经添加的ZONE</div>
-<div class="r_op"><a href=<?php echo $base_url."index.php/zone/add";?>>添加ZONE </a></div>
-<div class="clear"></div>
-<hr>
-<div class="clear"></div>
-
-<?php
-
-foreach($zones as $item){
-	echo "<div class=\"zone\"><a href=".$base_url."index.php/zone/manage/".$item['zone'].">".$item['zone']."</a></div><div class='fl'><a href=".$base_url."index.php/zone/del/".$item['zone']." onclick=\"return confirm('确定删除".$item['zone']."')\">删除</a></div>
-	<div class=\"bk\"></div>";
-}
+<?php 
+//judge
 $is_first_page=FALSE;
 $is_last_page=FALSE;
 $pre_page=$cur_page-1;
@@ -20,6 +10,24 @@ $nxt_page=$cur_page+1;
 if($nxt_page>=$max_page){
 	$is_last_page=TRUE;
 }
+?>
+<div class="meta">已经添加的ZONE</div>
+<div class="r_op"><a href=<?php echo $base_url."index.php/zone/add";?>>添加ZONE </a></div>
+<div class="clear"></div>
+<hr>
+<div class="clear"></div>
+<div class="pre_page" <?php if($is_first_page){echo 'style="display:none;';} ?>><a href="<?php echo $base_url.'index.php/zone/index/'.$pre_page;?>">上一页</a></div><div class="nxt_page" <?php if($is_last_page){echo 'style="display:none;';} ?>><a href="<?php echo $base_url.'index.php/zone/index/'.$nxt_page;?>">下一页</a></div>
+<?php
+
+foreach($zones as $item):
+?>
+<div class="zone">
+	<div class="zone_name"><a  href="<?php echo $base_url;?>index.php/zone/manage/<?php echo $item['zone']; ?>"><?php echo $item['zone']; ?></a></div>
+	<div class="del_zone"><a href="<?php echo $base_url; ?>index.php/zone/del/<?php echo $item['zone']; ?>" onclick="return confirm('确定删除<?php echo $item['zone']; ?>')">删除</a></div>
+</div>	
+<?php
+endforeach;
+
 ?>
 
 <div class="pre_page" <?php if($is_first_page){echo 'style="display:none;';} ?>><a href="<?php echo $base_url.'index.php/zone/index/'.$pre_page;?>">上一页</a></div><div class="nxt_page" <?php if($is_last_page){echo 'style="display:none;';} ?>><a href="<?php echo $base_url.'index.php/zone/index/'.$nxt_page;?>">下一页</a></div>
